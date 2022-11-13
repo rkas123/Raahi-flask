d7b6ff8af13533f4e64f0d3ed707916b5186b259
rkas123 <rkrupeshkumar345@gmail.com>
2022-11-13 18:50:22 +0530

    Configure POST requests for YOLO argument

--- app.py
+++ app.py
@@ -5,13 +5,144 @@ import numpy as np
 from PIL import Image
 import io
 import base64
+import os
+import time
 
 CANNY = 'canny'
 SEGMENTED = 'segmented'
 HOUGH = 'hough'
+YOLO = 'yolo'
 
 app = Flask(__name__)
 api = Api(app)
+
+#hyperparameters
+confthres=0.5
+nmsthres=0.1
+
+# declaring globally
+labelsPath=""
+cfgpath=""
+wpath=""
+Lables=''
+CFG=''
+Weights=''
+nets=''
+Colors=''
+
+def get_labels(labels_path):
+    # load the COCO class labels our YOLO model was trained on
+    #labelsPath = os.path.sep.join([yolo_path, "yolo_v3/coco.names"])
+    lpath=os.path.sep.join([labels_path])
+    LABELS = open(lpath).read().strip().split("\n")
+    return LABELS
+
+def get_colors(LABELS):
+    # initialize a list of colors to represent each possible class label
+    np.random.seed(42)
+    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),dtype="uint8")
+    return COLORS
+
+def get_weights(weights_path):
+    # derive the paths to the YOLO weights and model configuration
+    weightsPath = os.path.sep.join([weights_path])
+    return weightsPath
+
+def get_config(config_path):
+    configPath = os.path.sep.join([config_path])
+    return configPath
+
+def load_model(configpath,weightspath):
+    # load our YOLO object detector trained on COCO dataset (80 classes)
+    print("[INFO] loading YOLO from disk...")
+    net = cv2.dnn.readNetFromDarknet(configpath, weightspath)
+    return net
+
+def get_predection(image,net,LABELS,COLORS):
+    H = image.shape[0]
+    W = image.shape[1]
+
+    # determine only the *output* layer names that we need from YOLO
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    # construct a blob from the input image and then perform a forward
+    # pass of the YOLO object detector, giving us our bounding boxes and
+    # associated probabilities
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    # print(layerOutputs)
+    end = time.time()
+
+    # show timing information on YOLO
+    print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+    # initialize our lists of detected bounding boxes, confidences, and
+    # class IDs, respectively
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability) of
+            # the current object detection
+            scores = detection[5:]
+            # print(scores)
+            classID = np.argmax(scores)
+            # print(classID)
+            confidence = scores[classID]
+
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if confidence > confthres:
+                # scale the bounding box coordinates back relative to the
+                # size of the image, keeping in mind that YOLO actually
+                # returns the center (x, y)-coordinates of the bounding
+                # box followed by the boxes' width and height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                # use the center (x, y)-coordinates to derive the top and
+                # and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                # update our list of bounding box coordinates, confidences,
+                # and class IDs
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    # apply non-maxima suppression to suppress weak, overlapping bounding
+    # boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confthres,
+                            nmsthres)
+
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            # draw a bounding box rectangle and label on the image
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+            print(boxes)
+            print(classIDs)
+            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,0.5, color, 2)
+    return image
+
+
 
 @app.route('/')
 def index():
@@ -62,6 +193,7 @@ class Raahi(Resource):
         return 'donzoes'
     
     def post(self,routine): 
+        print(routine)
         file = request.files['image']
         image = Image.open(file)
         num = np.array(image) 
@@ -73,6 +205,12 @@ class Raahi(Resource):
         except: 
             return 'Failed to get image', 400
         
+        if routine == YOLO:
+            res = get_predection(num,nets,Lables,Colors)
+            # cv2.imshow("Image", res)
+            # cv2.waitKey()
+            return self.encode_image(res), 200
+
         canny_image = self.canny(num)
         if routine == CANNY:
             return self.encode_image(canny_image), 200
@@ -93,4 +231,20 @@ class Raahi(Resource):
 api.add_resource(Raahi,"/raahi/<string:routine>")
 
 if __name__ == "__main__":
+    #initializing once
+    labelsPath="yolov3/coco.names"
+    cfgpath="yolov3/yolo.cfg"
+    wpath="yolov3/yolo.weights"
+    Lables=get_labels(labelsPath)
+    CFG=get_config(cfgpath)
+    Weights=get_weights(wpath)
+    nets=load_model(CFG,Weights)
+    Colors=get_colors(Lables)
+
+    # res=get_predection(image,nets,Lables,Colors)
+    # image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    # show the output image
+    # cv2.imshow("Image", res)
+    # cv2.waitKey()
+
     app.run(debug=True, host = '0.0.0.0')
